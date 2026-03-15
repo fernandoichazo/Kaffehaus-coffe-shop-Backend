bee15caab77d8871c7d4c6e6185252a59f6ac8a1
Fernando Ichazo Cisneros <fernando.ichazo@ucb.edu.bo>
2026-03-15 18:06:47 -0400

    feat: Adding cliente jpa repository

---
+++ src/main/java/com/ucb/kaffehaus/personal/cliente/infrastructure/ClienteJpaRepository.java
@@ -0,0 +1,7 @@
+package com.ucb.kaffehaus.personal.cliente.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteJpaRepository extends JpaRepository<ClienteEntity, Integer>{
+
+}
