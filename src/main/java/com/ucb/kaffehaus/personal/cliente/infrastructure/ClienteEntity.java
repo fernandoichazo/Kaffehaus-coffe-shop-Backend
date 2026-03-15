35f88c2d299a7ba9f45726f48fc4dde6557bbdea
Fernando Ichazo Cisneros <fernando.ichazo@ucb.edu.bo>
2026-03-15 18:03:10 -0400

    feat: Adding cliente database entity

---
+++ src/main/java/com/ucb/kaffehaus/personal/cliente/infrastructure/ClienteEntity.java
@@ -0,0 +1,38 @@
+package com.ucb.kaffehaus.personal.cliente.infrastructure;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class ClienteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "borrado", nullable = false)
+    private boolean borrado;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
