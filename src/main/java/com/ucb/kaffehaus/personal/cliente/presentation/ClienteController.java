84e937299de885b64a9cf7fe1502e4d6a763a775
Fernando Ichazo Cisneros <fernando.ichazo@ucb.edu.bo>
2026-03-15 18:22:29 -0400

    feat: adding cliente controller

---
+++ src/main/java/com/ucb/kaffehaus/personal/cliente/presentation/ClienteController.java
@@ -0,0 +1,10 @@
+package com.ucb.kaffehaus.personal.cliente.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteController {
+
+}
