202b9775235a7d7f6ceb8b0eb90137af4975e357
Fernando Ichazo Cisneros <fernando.ichazo@ucb.edu.bo>
2026-04-20 18:26:55 -0400

    feat: Using ErrorResponse from errors

--- src/main/java/com/ucb/kaffehaus/shared/presentation/GlobalExceptionHandler.java
+++ src/main/java/com/ucb/kaffehaus/shared/presentation/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.ucb.kaffehaus.shared.presentation;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.ucb.kaffehaus.shared.application.error.CustomException;
+import com.ucb.kaffehaus.shared.application.exception.ErrorResponse;
 import com.ucb.kaffehaus.shared.application.exception.ValidationErrorResponse;
 import com.ucb.kaffehaus.shared.application.exception.ValidationException;
 
@@ -28,20 +27,17 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler(CustomException.class)
-	public ResponseEntity<ValidationErrorResponse> handleCustomException(CustomException exception) {
+	public ResponseEntity<ErrorResponse> handleCustomException(CustomException exception) {
 		HttpStatus httpStatus = HttpStatus.resolve(exception.getStatusCode());
 		if (httpStatus == null) {
 			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
 
-		Map<String, String> errors = new HashMap<>();
-		errors.put("customException", exception.getMessage());
-
-		ValidationErrorResponse errorResponse = new ValidationErrorResponse(
+		ErrorResponse errorResponse = new ErrorResponse(
 				LocalDateTime.now(),
 				httpStatus.value(),
-				"Error",
-				errors);
+				exception.getMessage()
+        );
 
 		return ResponseEntity.status(httpStatus).body(errorResponse);
 	}
