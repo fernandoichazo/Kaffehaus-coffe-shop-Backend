237ea7411a98900399c8f373b4e378c08936cbf0
Fernando Ichazo Cisneros <fernando.ichazo@ucb.edu.bo>
2026-05-14 17:02:13 -0400

    refactor: changing jpa entity client

--- src/main/java/com/ucb/kaffehaus/personal/cliente/infrastructure/ClienteEntity.java
+++ src/main/java/com/ucb/kaffehaus/personal/cliente/infrastructure/ClienteEntity.java
@@ -1,11 +1,17 @@
 package com.ucb.kaffehaus.personal.cliente.infrastructure;
 
 import java.time.LocalDateTime;
+
+import com.ucb.kaffehaus.personal.persona.infrastructure.PersonaEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -13,18 +19,30 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "cliente")
 public class ClienteEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_persona", nullable = false, unique = true)
+    private PersonaEntity persona;
+
     @Column(name = "borrado", nullable = false)
     private boolean borrado;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    protected ClienteEntity() {}
+
+    public ClienteEntity(PersonaEntity persona) {
+        this.persona = persona;
+        this.borrado = false;
+    }
 
     @PrePersist
     protected void onCreate() {
@@ -35,4 +53,13 @@ public class ClienteEntity {
     protected void onUpdate() {
         this.updatedAt = LocalDateTime.now();
     }
+
+    public Integer getId() {return id;}
+    public void setId(Integer id) {this.id = id;}
+    public PersonaEntity getPersona() {return persona;}
+    public void setPersona(PersonaEntity persona) {this.persona = persona;}
+    public boolean isBorrado() {return borrado;}
+    public void setBorrado(boolean borrado) {this.borrado = borrado;}
+    public LocalDateTime getCreatedAt() {return createdAt;}
+    public LocalDateTime getUpdatedAt() {return updatedAt;}
 }
