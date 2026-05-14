94a6e6e5b8f37a9194301dfe99684ff1f8dd221e
Fernando Ichazo Cisneros <fernando.ichazo@ucb.edu.bo>
2026-05-14 19:06:56 -0400

    feat: Adding categoriaentity jpa for persistence

---
+++ src/main/java/com/ucb/kaffehaus/inventario/categoria/infrastructure/CategoriaEntity.java
@@ -0,0 +1,34 @@
+package com.ucb.kaffehaus.inventario.categoria.infrastructure;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categoria")
+public class CategoriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "borrado", nullable = false)
+    private boolean borrado;
+
+    protected CategoriaEntity() {}
+
+    public CategoriaEntity(String nombre) {
+        this.nombre = nombre;
+        this.borrado = false;
+    }
+
+    public UUID getId() {return id;}
+    public void setId(UUID id) {this.id = id;}
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public boolean isBorrado() {return borrado;}
+    public void setBorrado(boolean borrado) {this.borrado = borrado;}
+}
